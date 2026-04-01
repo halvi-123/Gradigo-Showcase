08373a364db746bc95ef50088fd8416bcdc33fd7
Mohamed Kordofani <minetubehdtv@gmail.om>
2026-04-01 18:05:59 +0100

    settings added calculators

--- Backend/first_job_navigator/settings.py
+++ Backend/first_job_navigator/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'apps.accounts',
-    'apps.budget'
+    'apps.budget',
+    'apps.calculators'
 ]
 
 MIDDLEWARE = [
