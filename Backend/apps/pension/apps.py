ce7ad009d48d7a5a5d39ae476bfa06525b7f9178
Abdul Malik <am05550@surrey.ac.uk>
2026-04-03 17:58:17 +0100

    feat: added and registered pension app

--- Backend/apps/pension/apps.py
+++ Backend/apps/pension/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PensionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "pension"
+    name = "apps.pension"
--- Backend/first_job_navigator/settings.py
+++ Backend/first_job_navigator/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "apps.accounts",
     "apps.budget",
     "apps.calculators",
+    "apps.pension",
     'drf_spectacular',
 ]
 
