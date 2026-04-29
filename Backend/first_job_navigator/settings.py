479ef39f80ea7fa22d35544d4948ea6add813bf5
Hassaan <hassaanalvi15@gmail.com>
2026-04-29 17:50:40 +0100

    fixed CI secret key env

--- Backend/first_job_navigator/settings.py
+++ Backend/first_job_navigator/settings.py
@@ -25,7 +25,10 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "unsafe-dev-secret-key"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
